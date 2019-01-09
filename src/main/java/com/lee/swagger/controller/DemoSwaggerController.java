9b83e2fa7202c28567bf9a7506a865744a34c048
JiaxiLee <jiaxiwuli@126.com>
2019-01-09 09:54:38 +0800

    springboot集成swagger一个小demo 使用了springboot热部署方式

---
+++ src/main/java/com/lee/swagger/controller/DemoSwaggerController.java
@@ -0,0 +1,29 @@
+package com.lee.swagger.controller;
+
+import com.lee.swagger.entity.Student;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api")
+@Api("学生信息查询API接口" )
+@RestController("测试Controller")
+public class DemoSwaggerController {
+
+
+    @ApiOperation(value = "根据id查询学生信息", notes = "查询数据库中某个学生的信息")
+    @ApiImplicitParam(name = "id", value = "学生id", paramType = "path", required = true, dataType = "Integer")
+    @RequestMapping(value = "{/id}", method = RequestMethod.GET)
+    public Student getById(Integer id) {
+        Student student = new Student();
+        student.setId(1l);
+        student.setName("zhangsan==============-------------");
+        student.setAge(14);
+        return student;
+    }
+
+}

135ede0d50e7893c97d3c26f3090f1dd55eb8b08
lijiaxi <jiaxilee@126.com>
2019-01-23 21:58:23 +0800

    修改了参数方式

--- src/main/java/com/lee/swagger/controller/DemoSwaggerController.java
+++ src/main/java/com/lee/swagger/controller/DemoSwaggerController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +18,13 @@ public class DemoSwaggerController {
 
     @ApiOperation(value = "根据id查询学生信息", notes = "查询数据库中某个学生的信息")
     @ApiImplicitParam(name = "id", value = "学生id", paramType = "path", required = true, dataType = "Integer")
-    @RequestMapping(value = "{/id}", method = RequestMethod.GET)
+    //@RequestMapping(value = "{/id}", method = RequestMethod.GET)
+    @GetMapping("{/id}")
     public Student getById(Integer id) {
         Student student = new Student();
         student.setId(1l);
         student.setName("zhangsan==============-------------");
+        System.out.println(id+"===========");
         student.setAge(14);
         return student;
     }
